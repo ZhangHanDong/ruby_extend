48f9baa6b57688e948a8794f31e395165e7928af
Alex <blackanger.z@gmail.com>
2010-10-26 23:17:20 +0800

    modify

---
+++ lib/ruby_extend/hash_extendsions.rb
@@ -0,0 +1,21 @@
+module RubyExtend
+  module HashExtendsions
+    def self.included(base)
+      base.send :include, InstanceMethods
+    end
+
+    module InstanceMethods
+      def hash_reverse
+        hash_new = {}
+        self.each {|key,value|
+          if not hash_new.has_key?(key) then hash_new[value] = key end
+        }
+        return hash_new
+      end
+    end
+  end
+end
+
+class Hash
+  include RubyExtend::HashExtendsions
+end
---
+++ lib/ruby_extend/version.rb
@@ -0,0 +1,3 @@
+module RubyExtend
+  VERSION = "0.0.1"
+end
