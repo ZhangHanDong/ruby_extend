5780c24c0f37e297b1f1cf2ec739d1736b658e9b
Alex <blackanger.z@gmail.com>
2010-12-30 18:11:04 +0800

    modify

--- lib/ruby_extendsions/array_extendsions.rb
+++ lib/ruby_extendsions/array_extendsions.rb
@@ -13,6 +13,15 @@ module RubyExtendsions
       def fill_nil!(val)
         self.each_index { |i| self[i] ||= val }
       end
+      
+      #change [[1,2,3][4,5,6]] to ('1','2','3'),('4','5','6')
+      #be used with mess insert mysql
+      def format_mess_insert
+        self.inject('') do |s,i|
+          t = i.inject('(') { |x, n| x << "'#{n}'," }.chop
+          s << t << '),'
+        end.chop
+      end
     end#InstanceMethods
     
     module ClassMethods
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "RubyExtend::ArrayExtendsions" do
   before(:each) do
     @arr1 = [1, 2, 3, nil, nil, nil]
+    @arr2 = [[1,2,3,nil,4],[4,5,6,nil,nil]]
   end
 
   it "A array data should have not fill_nil method if havn't use ruby_extend " do
@@ -13,6 +14,11 @@ describe "RubyExtend::ArrayExtendsions" do
     require 'ruby_extendsions'
     @arr1.fill_nil!('')
     @arr1.should eql([1,2,3,'','',''])
+  end
+  
+  it "format_mess_insert should be successful!" do
+    require 'ruby_extendsions'
+    @arr2.format_mess_insert.should eql("('1','2','3','','4'),('4','5','6','','')")
   end
   
 end
